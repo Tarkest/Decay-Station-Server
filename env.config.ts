9256f06332544e28f96612ff865a080c172a277e
Vadim Zinchenko <v.zinchenko@futuro.team>
2020-06-08 03:53:07 +0300

    Env updated

--- env.config.ts
+++ env.config.ts
@@ -30,6 +30,38 @@ const config = {
     },
     logging: true,
     migrationsRun: true
+  },
+  stage: {
+    type: "mysql",
+    host: "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    username: "raz0hrf6xfrnbj63",
+    password: "pc0ohlf98zuz77le",
+    database: "eblgnvo8ssprsukj",
+    synchronize: false,
+    port: 3306,
+    entities: ["dist/database/models/**/*.js"],
+    migrations: ["dist/database/migration/*.js"],
+    cli: {
+      "migrationsDir": "migration"
+    },
+    logging: false,
+    migrationsRun: true
+  },
+  production: {
+    type: "mysql",
+    host: "sq65ur5a5bj7flas.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    username: "gr6b6aeug676zecg",
+    password: "o6ulnu8t9hzswkpt",
+    database: "ncc4t5ui4s4ql5ve",
+    synchronize: false,
+    port: 3306,
+    entities: ["dist/database/models/**/*.js"],
+    migrations: ["dist/database/migration/*.js"],
+    cli: {
+      "migrationsDir": "migration"
+    },
+    logging: false,
+    migrationsRun: true
   }
 }
 
